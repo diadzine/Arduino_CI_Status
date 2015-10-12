6aa303f796bae7850562e08aec4907d622b5ecfe
Aymeric Bringard <contact@aymericb.com>
2015-10-12 20:32:25 +0200

    add jenkins api to poll projects

--- software/python/main.py
+++ software/python/main.py
@@ -5,11 +5,17 @@ import logging
 import sys
 import time
 import signal
-import random
+from jenkinsapi.jenkins import Jenkins, Requester
+import requests
 
 
-logging.basicConfig(filename='logfile',level=logging.DEBUG)
+if getattr(settings, 'DEBUG', False):
+    logging.basicConfig(filename='logfile', level=logging.DEBUG)
+else:
+    logging.basicConfig(filename='logfile', level=logging.ERROR)
 logger = logging.getLogger('status')
+
+servers = dict()
 
 
 def signal_term_handler(signal, frame):
@@ -19,13 +25,33 @@ def signal_term_handler(signal, frame):
 signal.signal(signal.SIGTERM, signal_term_handler)
 
 
+def get_jenkins_instance(server):
+    try:
+        if server.get('baseurl', '').split('//')[1] in servers:
+            return servers[server.get('baseurl', '').split('//')[1]]
+
+        connection = Jenkins(
+            server.get('baseurl', ''),
+            requester=Requester(
+                username=server.get('username', ''),
+                password=server.get('pass_or_token', ''),
+                **server.get('extra', None)
+            )
+        )
+        servers[server.get('baseurl', '').split('//')[1]] = connection
+        return connection
+    except requests.HTTPError as hte:
+        logger.exception(hte)
+        logger.warn("Failed to connect to {}".format(server.get('url', '')))
+        raise
+
 
 def open_serial_connection():
     try:
         device = getattr(settings, 'DEVICE', None)
         ser = serial.Serial(device.get('PORT', ''), device.get('BAUDRATE', ''))
         logger.debug(ser)
-    except serial.SerialException as se:
+    except serial.SerialException:
         logger.error(
             "Cannot open serial connection to {} at {}".format(
                 device.get('PORT', ''),
@@ -41,8 +67,30 @@ def poll_projects(ser):
 
     for project in projects:
         if project.get('active', False):
-            cmd = random.choice(['OK', 'WARNING', 'ERROR', 'UNKNOWN'])
-            cmd_string = '{} {}\r\n'.format(cmd, project.get('id'))
+            server = project.get('server', None)
+            connection = get_jenkins_instance(server)
+            jobname = server.get('job', None)
+
+            logger.debug(connection)
+
+            if connection.has_job(jobname):
+                job = connection.get_job(jobname)
+
+                if not job.is_enabled():
+                    cmd_string = '{} {}\r\n'.format("OFF", project.get('id'))
+                else:
+                    lb = job.get_last_build()
+
+                    if lb.is_good():
+                        cmd_string = '{} {}\r\n'.format(
+                            "SUCCESS",
+                            project.get('id')
+                        )
+                    else:
+                        cmd_string = '{} {}\r\n'.format(
+                            "FAILED",
+                            project.get('id')
+                        )
         else:
             cmd_string = 'OFF {}\r\n'.format(project.get('id'))
 
